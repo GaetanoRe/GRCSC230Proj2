1bbe73bd8cfc297074a313c6fb66a7edaccb8809
GaetanoRe <gaetanore@live.com>
2025-11-03 22:30:02 -0500

    Finishing implementation of AVL tree

--- src/basketballApp/AVLTree.java
+++ src/basketballApp/AVLTree.java
@@ -9,44 +9,140 @@ package basketballApp;
 public class AVLTree<E extends Comparable<E>> implements Tree<E>{
 
 	private Node<E> root;
-	
-	public AVLTree(){
-		root = null;
-	}
+
 	@Override
 	public Tree<E> insert(E data) {
-		// TODO Auto-generated method stub
-		return null;
+		root = insert(data, root);
+		return this;
 	}
 
 	@Override
 	public void delete(E data) {
-		// TODO Auto-generated method stub
-		
+		root = delete(data, root);
 	}
 
 	@Override
 	public void traverse() {
-		// TODO Auto-generated method stub
-		
+		traverseInOrder(root);
+
 	}
 
 	@Override
 	public E getMax() {
-		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty()) {
+			return null;
+		}
+		return getMax(root);
 	}
 
 	@Override
 	public E getMin() {
-		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty()) {
+			return null;
+		}
+		return getMin(root);
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.root == null;
+	}
+	
+	private void traverseInOrder(Node<E> node) {
+		if(node != null) {
+			traverseInOrder(node.getLeftChild());
+			System.out.println(node);
+			traverseInOrder(node.getRightChild());
+		}
+	}
+	
+	
+	
+	private void traversePreOrder(Node<E> node) {
+		
+	}
+	
+	private void traversePostOrder(Node<E> node) {
+		
+	}
+	
+	private E getMax(Node<E> node){
+		if(node.getRightChild() != null) {
+			return getMax(node.getRightChild());
+		}
+		else {
+			return node.getData();
+		}
+	}
+	
+	private E getMin(Node<E> node) {
+		if(node.getLeftChild() != null) {
+			return getMin(node.getLeftChild());
+		}
+		else {
+			return node.getData();
+		}
+	}
+	
+	private Node<E> insert(E data, Node<E> node) {
+		if(node == null) {
+			return new Node<E>(data);
+		}
+		else if(data.compareTo(node.getData()) < 0) {
+			node.setLeftChild(insert(data, node.getLeftChild()));
+		}
+		else if(data.compareTo(node.getData()) > 0) {
+			node.setRightChild(insert(data, node.getRightChild()));
+		}
+		else {
+			return node;
+		}
+		
+		updateHeight(node);
+		return applyRotation(node);
+		
+	}
+	
+	private Node<E> delete(E data, Node<E> node){
+		if(node == null) {
+			return null;
+		}
+		if(data.compareTo(node.getData()) < 0) {
+			node.setLeftChild(delete(data, node.getLeftChild()));
+		}
+		else if(data.compareTo(node.getData()) > 0) {
+			node.setRightChild(delete(data, node.getRightChild()));
+		}else {
+			if(node.getLeftChild() == null) {
+				return node.getRightChild();
+			}
+			else if(node.getRightChild() == null) {
+				return node.getLeftChild();
+			}
+			node.setData(getMax(node.getLeftChild()));
+			node.setLeftChild(delete(node.getData(), node.getLeftChild()));
+			
+		}
+		
+		return node;
+	}
+	
+	private void updateHeight(Node<E> node) {
+		
+	}
+	
+	private Node<E> applyRotation(Node<E> node){
+		
+	}
+	
+	private Node<E> rotateLeft(Node<E> node){
+		
+	}
+	
+	private Node<E> rotateRight(Node<E> node){
+		Node<E> z = node;
+		Node<E> y = node.getLeftChild();
+		Node<E> x = y.getLeftChild();
 	}
 	
 	
